f17387bddbd56ba644fec2a7c88cacd72f320078
Germain Salvato Vallverdu <germain.vallverdu@gmail.com>
2022-04-07 10:43:57 +0200

    package the whole app

--- src/toyapp/app.py
+++ src/toyapp/app.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from gui.main import main
+import sys
+from PyQt5.QtWidgets import QApplication
+from .gui import Toyapp
+
+
+def main():
+    """ Main application """
+    app = QApplication([])
+
+    myapp = Toyapp()
+    myapp.show()
+    sys.exit(app.exec())
+
 
 if __name__ == "__main__":
     main()
