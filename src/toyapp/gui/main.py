5d31ad9bded21f7f26a66aba9dba3cf6fe051c3b
Germain Salvato Vallverdu <germain.vallverdu@gmail.com>
2022-04-07 10:39:39 +0200

    reorganize folders

---
+++ src/toyapp/gui/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+from PyQt5.QtWidgets import QApplication
+from .toyapp import Toyapp
+
+def main():
+    """ Main application """
+    app = QApplication([])
+
+    myapp = Toyapp()
+    myapp.show()
+    sys.exit(app.exec())
